cac4e44e51f9795832ed27acd94f8cb63f7c1628
Enrique Molinari <enrique.molinari@gmail.com>
2026-08-08 21:04:31 -0300

    moving to hibernate 7.4

--- src/main/java/unrn/main/Main.java
+++
@@ -1,9 +0,0 @@
-package unrn.main;
-
-@SpringBootApplication
-@ComponentScan(basePackages = {"unrn.*"})
-public class Main {
-    public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
-    }
-}
